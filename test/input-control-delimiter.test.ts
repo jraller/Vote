3cf13cdf994e54d492ac0493077d8f379d5ccf8f
Jason Aller <jraller@gmail.com>
2017-05-04 17:12:56 -0700

    much to learn, it will be worth it

--- test/input-control-delimiter.test.ts
+++ test/input-control-delimiter.test.ts
@@ -4,8 +4,13 @@ import {mount} from 'avoriaz';
 const Delimiter = require('!vue-loader!./../app/views/input-control-delimiter.vue');
 
 describe('sanity', () => {
+	const wrapper = mount(Delimiter);
 	it('has the right name', () => {
-		const wrapper = mount(Delimiter);
 		expect(wrapper.name()).to.equal('inputControlDelimiter');
 	});
+	it('can get description', () => {
+		expect(wrapper.vm.getDescription('tab')).to.equal('Tab');
+	});
 });
+
+// https://github.com/jraller/Vote/tree/webpack
