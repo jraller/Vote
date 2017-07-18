0d2aa92519d00f6de2b6ebb41fc394ae95fe513a
Jason Aller <jraller@gmail.com>
2017-07-18 13:36:52 -0700

    updating for new wallaby functionality

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,28 +1,40 @@
 const webpackConfig = require('./webpack.config')('test');
 const wallabyWebpack = require('wallaby-webpack');
 
-webpackConfig.module.rules = webpackConfig.module.rules.filter(r => !'.ts'.match(r.test) && !'.js'.match(r.test));
-webpackConfig.resolve.extensions = ['.vue', '.jsx', '.js', '.json'];
-// json only needed for brittleness in Chai, remove when fixed
-delete webpackConfig.devServer;
-delete webpackConfig.entry;
 
 // https://wallabyjs.com/docs/integration/webpack.html
 
-module.exports = function () {
+module.exports = function (wallaby) {
+
+	delete webpackConfig.devServer;
+	delete webpackConfig.entry;
+	// json only needed for brittleness in Chai, remove when fixed
+	webpackConfig.resolve.extensions = ['.vue', '.jsx', '.js', '.json'];
+
+	webpackConfig.module.rules = webpackConfig.module.rules.filter(r => !'.ts'.match(r.test) && !'.js'.match(r.test));
+	webpackConfig.module.rules.find(r => r.loader === 'vue-loader').options.loaders.js = '';
+
+	webpackConfig.resolve.alias = {'@': require('path').join(wallaby.projectCacheDir, 'app')};
+	// webpackConfig.externals = {vue: 'Vue'};
 
 	const wallabyPostprocessor = wallabyWebpack(webpackConfig);
 
 	return {
 		debug: true,
+		compilers: {
+			'**/*.js': wallaby.compilers.babel({}),
+			'**/*.ts': wallaby.compilers.typeScript({}),
+			'**/*.vue': require('wallaby-vue-compiler')(wallaby.compilers.babel({}))
+		},
 		env: {
-			type: 'browser', // browser or node
+			type: 'browser' // browser or node
 			// runner: 'node',
 		},
 		files: [
 			{pattern: 'tsconfig.json', load: false, instrument: false},
+			{pattern: 'node_modules/vue/dist/vue.js', instrument: false},
 			{pattern: 'app/**/*.ts', load: false},
-			{pattern: 'app/**/*.vue', load: false, instrument: false},
+			{pattern: 'app/**/*.vue', load: false}
 		],
 		hints: {
 			ignoreCoverage: /ignore coverage/
@@ -30,11 +42,15 @@ module.exports = function () {
 		postprocessor: wallabyPostprocessor,
 		setup: function () {
 			// window.expect = chai.expect;
+			// eslint-disable-next-line
+			Vue.config.errorHandler = function (err) {
+				throw err;
+			};
 			window.__moduleBundler.loadTests();
 		},
 		testFramework: 'mocha',
 		tests: [
 			{pattern: 'test/**/*.test.ts', load: false}
-		],
+		]
 	};
 };
